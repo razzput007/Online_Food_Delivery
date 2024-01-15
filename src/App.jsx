6527dd0fa471828cdf14380ce2e3b2e51df2e45d
razzput007 <dhoom.rk74@gmail.com>
2024-01-15 23:02:24 +0530

    Adding Carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import {Routes,Route} from "react-router-dom";
 import Home from './pages/Home'
 import Login from "./pages/Login";
-
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle"
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
 function App() {
 
   return (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import Card from "../components/Card";
+import Carousel from "../components/Carousel";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
@@ -6,11 +7,17 @@ export default function Home() {
 
   return (
     <div>
+      <div>
       <Navbar />
+      </div>
+      <div><Carousel/></div>
       <div>
         <Card/>
       </div>
+      <div>
       <Footer />
+      </div>
+      
     </div>
   );
 }
